69715a91333b6df88102b6278457fd568ac57e0a
Deepanshu Mishra <d4deepanshu723@gmail.com>
2026-06-12 18:31:24 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,4 +1,4 @@
-def clone(String url , String branch) {
+def Clone(String url , String branch) {
   echo "Cloning the git repository"
   git url:"${url}", branch:"${branch}"
   echo "Clone successfull"
