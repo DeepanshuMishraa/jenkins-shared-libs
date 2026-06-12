d5268ff385418279811b4cfa9089c249e74dc8af
Deepanshu Mishra <d4deepanshu723@gmail.com>
2026-06-12 18:39:06 +0530

    Update and rename Clone.groovy to clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,4 +1,4 @@
-def clone(String url , String branch) {
+def call(String url , String branch) {
   echo "Cloning the git repository"
   git url:"${url}", branch:"${branch}"
   echo "Clone successfull"
